1fef03d95b6ddbb66bd45ea2f2877f8689696ba1
Palash Mandal <palash15-2092@diu.edu.bd>
2022-10-01 22:04:55 +0600

    print name

---
+++ OOPExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.firstName = "Mr.";
+            person1.middleName = "Palash";
+            person1.lastName = "Mandal";
+
+            string fullName = person1.GetFullName();
+            Console.WriteLine(fullName);
+            Console.ReadKey();
+
+        }
+    }
+}
